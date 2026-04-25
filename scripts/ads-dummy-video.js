e99a5376f00806458af91b1195a891928f10c571
YoshiJunior <163933549+YoshiJunior@users.noreply.github.com>
2026-04-25 07:54:24 -0500

    Remove empty interstitial ad functions
    Removed unused functions related to interstitial ads.

--- scripts/ads-dummy-video.js
+++ scripts/ads-dummy-video.js
@@ -1,13 +1,1 @@
-'use strict';
 
-function showInterstitial(audioOn, interstitialType, interstitialName)
-{
-}
-
-function tryInitRewardedInterstitial(audioOn)
-{
-}
-
-function tryShowRewardedInterstitial(audioOn)
-{
-}
